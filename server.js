c0ef515f223111954a38d261ed2dfa3cd45f69b6
pulopr <pulopr95@gmail.com>
2026-08-12 21:17:39 +0200

    Usar puerto dinamico para el despliegue

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // --- Umbrales del veredicto (los que definimos) ---
 function calcularEstado(jugadores) {
